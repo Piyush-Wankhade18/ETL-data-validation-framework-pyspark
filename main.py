f5f5569789a8aafaaa9baf7ef4c63dc596c59b46
Piyush Wankhade <piyush.wankhade19@gmail.com>
2026-04-13 16:23:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from src.utils.spark_session import get_spark
 from src.utils.logger import get_logger
+from src.utils.config_reader import load_config
 
 from src.validation.row_count import validate_row_count
 from src.validation.data_compare import validate_data
@@ -8,11 +9,19 @@ from src.validation.null_check import validate_nulls
 from src.validation.duplicate_check import validate_duplicates
 from src.validation.aggregate_check import validate_aggregate
 
+
+
 spark = get_spark()
 logger = get_logger()
 
-df_source = spark.read.table("source_db.table")
-df_target = spark.read.table("target_db.table")
+config = load_config()
+
+
+spark = get_spark()
+logger = get_logger()
+
+df_source = spark.read.table(config["source"]["name"])
+df_target = spark.read.table(config["target"]["name"])
 
 validate_row_count(df_source, df_target, logger)
 validate_data(df_source, df_target, logger)
